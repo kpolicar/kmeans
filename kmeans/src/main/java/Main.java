da00fa74e89a188db0894c0dc11e2596fb490d83
Klemen Poličar <klemen.janez.policar@gmail.com>
2021-01-10 16:13:07 +0100

    display waypoints on map

--- kmeans/src/main/java/Main.java
+++ kmeans/src/main/java/Main.java
@@ -1,11 +1,36 @@
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.jxmapviewer.JXMapViewer;
+import org.jxmapviewer.OSMTileFactoryInfo;
+import org.jxmapviewer.cache.FileBasedLocalCache;
+import org.jxmapviewer.input.CenterMapListener;
+import org.jxmapviewer.input.PanKeyListener;
+import org.jxmapviewer.input.PanMouseInputListener;
+import org.jxmapviewer.input.ZoomMouseWheelListenerCursor;
+import org.jxmapviewer.viewer.DefaultTileFactory;
+import org.jxmapviewer.viewer.GeoPosition;
+import org.jxmapviewer.viewer.TileFactoryInfo;
+import org.jxmapviewer.viewer.WaypointPainter;
 
+import javax.swing.*;
+import javax.swing.event.MouseInputListener;
+import java.awt.*;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.io.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
+        main2(args);
+    }
+
+    public static PopulationDataPoint[] dataPoints() {
         InputStream is = Main.class.getClassLoader().getResourceAsStream("data.json");
         Reader fr = new InputStreamReader(is);
 
@@ -13,8 +38,93 @@ public class Main {
         builder.setPrettyPrinting();
 
         Gson gson = builder.create();
-        PopulationDataPoint[] dataPoint = gson.fromJson(fr, PopulationDataPoint[].class);
+        return gson.fromJson(fr, PopulationDataPoint[].class);
+    }
 
-        System.out.println(dataPoint[0].name);
+    public static void main2(String[] args)
+    {
+        // Create a TileFactoryInfo for OpenStreetMap
+        TileFactoryInfo info = new OSMTileFactoryInfo();
+        DefaultTileFactory tileFactory = new DefaultTileFactory(info);
+
+        // Setup local file cache
+        File cacheDir = new File(System.getProperty("user.home") + File.separator + ".jxmapviewer2");
+        tileFactory.setLocalCache(new FileBasedLocalCache(cacheDir, false));
+
+        // Setup JXMapViewer
+        final JXMapViewer mapViewer = new JXMapViewer();
+        mapViewer.setTileFactory(tileFactory);
+
+        List<GeoPosition> dataPointPositions = Arrays
+                .stream(dataPoints())
+                .map(dataPoint -> new GeoPosition(dataPoint.la, dataPoint.lo))
+                .collect(Collectors.toList());
+
+        // Set the focus
+        mapViewer.setZoom(10);
+        mapViewer.setAddressLocation(dataPointPositions.stream().findFirst().get());
+
+        // Add interactions
+        MouseInputListener mia = new PanMouseInputListener(mapViewer);
+        mapViewer.addMouseListener(mia);
+        mapViewer.addMouseMotionListener(mia);
+
+        mapViewer.addMouseListener(new CenterMapListener(mapViewer));
+
+        mapViewer.addMouseWheelListener(new ZoomMouseWheelListenerCursor(mapViewer));
+
+        mapViewer.addKeyListener(new PanKeyListener(mapViewer));
+
+
+        Set<MyWaypoint> waypoints = new HashSet<MyWaypoint>(
+                dataPointPositions
+                        .stream()
+                        .map(geoPosition -> new MyWaypoint(Color.RED, geoPosition))
+                        .collect(Collectors.toList())
+        );
+
+        WaypointPainter<MyWaypoint> waypointPainter = new WaypointPainter<MyWaypoint>();
+        waypointPainter.setWaypoints(waypoints);
+        waypointPainter.setRenderer(new FancyWaypointRenderer());
+        mapViewer.setOverlayPainter(waypointPainter);
+
+        // Display the viewer in a JFrame
+        final JFrame frame = new JFrame();
+        frame.setLayout(new BorderLayout());
+        String text = "Use left mouse button to pan, mouse wheel to zoom and right mouse to select";
+        frame.add(new JLabel(text), BorderLayout.NORTH);
+        frame.add(mapViewer);
+        frame.setSize(800, 600);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+        mapViewer.addPropertyChangeListener("zoom", new PropertyChangeListener()
+        {
+            @Override
+            public void propertyChange(PropertyChangeEvent evt)
+            {
+                updateWindowTitle(frame, mapViewer);
+            }
+        });
+
+        mapViewer.addPropertyChangeListener("center", new PropertyChangeListener()
+        {
+            @Override
+            public void propertyChange(PropertyChangeEvent evt)
+            {
+                updateWindowTitle(frame, mapViewer);
+            }
+        });
+
+        updateWindowTitle(frame, mapViewer);
+    }
+
+    protected static void updateWindowTitle(JFrame frame, JXMapViewer mapViewer)
+    {
+        double lat = mapViewer.getCenterPosition().getLatitude();
+        double lon = mapViewer.getCenterPosition().getLongitude();
+        int zoom = mapViewer.getZoom();
+
+        frame.setTitle(String.format("JXMapviewer2 Example 3 (%.2f / %.2f) - Zoom: %d", lat, lon, zoom));
     }
 }
---
+++ kmeans/src/main/java/MyWaypoint.java
@@ -0,0 +1,32 @@
+import java.awt.Color;
+
+import org.jxmapviewer.viewer.DefaultWaypoint;
+import org.jxmapviewer.viewer.GeoPosition;
+
+/**
+ * A waypoint that also has a color and a label
+ * @author Martin Steiger
+ */
+public class MyWaypoint extends DefaultWaypoint
+{
+    private final Color color;
+
+    /**
+     * @param color the color
+     * @param coord the coordinate
+     */
+    public MyWaypoint(Color color, GeoPosition coord)
+    {
+        super(coord);
+        this.color = color;
+    }
+
+    /**
+     * @return the color
+     */
+    public Color getColor()
+    {
+        return color;
+    }
+
+}
